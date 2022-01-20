c32dfaae5358d7087f5b9eff9ed803fb333712e3
techinee <6206021621066@fitm.kmutnb.ac.th>
2022-01-20 16:43:53 +0700

    เขียนฟังก์ชันปุ่ม calculate

--- app/src/main/java/com/bbgun/changedistanceapplication/MainActivity.java
+++ app/src/main/java/com/bbgun/changedistanceapplication/MainActivity.java
@@ -3,12 +3,34 @@ package com.bbgun.changedistanceapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText kiloText;
+    private Button btnCal;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //(EditText) => new EditText(); //อ้างอิงวัตถุ R.id
+        kiloText = (EditText) findViewById(R.id.kiloText);
+        btnCal = (Button) findViewById(R.id.btnCal);
+
+        btnCal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                float kilo,mile;
+                kilo = Integer.parseInt(kiloText.getText().toString());
+                mile = kilo * 5 / 8;
+                Toast.makeText(MainActivity.this , mile + "miles." , Toast.LENGTH_LONG).show();
+            }
+        });
+
     }
 }
